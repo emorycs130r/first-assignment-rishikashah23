4f649faeab9651692b2da37198d781ec184fd134
rishikashah23 <78391510+rishikashah23@users.noreply.github.com>
2021-02-05 18:01:20 -0500

    Updated

--- easy_assign.py
+++ easy_assign.py
@@ -16,19 +16,35 @@ DO NOT EDIT THE FUNCTION NAMES.
 
 def append_two_strings(string_1, string_2):
 
-    pass
+    result = string_1 + string_2
+    return result
 
 
 def append_character(string_1, char_1):
 
-    pass
+    result = string_1 + char_1
+    return result
 
 
 def append_num_to_string(string_1, num_1):
 
-    pass
+    result = string_1 + num_1
+    return result
 
 
 if __name__ == "__main__":
     
-    pass
+    val_11 = input("Enter a string: ")
+    val_12 = input("Enter another string: ")
+    val_1 = append_two_strings(val_11, val_12)
+    print(val_1)
+
+    val_21 = input("Enter a string: ")
+    val_22 = input("Enter character: ")
+    val_2 = append_character(val_21, val_22)
+    print(val_2)
+
+    val_31 = input("Enter a string: ")
+    val_32 = input("Enter a number: ")
+    val_3 = append_num_to_string(val_31, val_32)
+    print(val_3)
--- medium_assign.py
+++ medium_assign.py
@@ -12,16 +12,16 @@ DO NOT EDIT THE FUNCTION NAMES.
 
 
 '''
+import math
 
 def expression_1(x):
 
     '''
-    
         Write a code that returns value for the following expression: x^3 - (2x + x^2) - 100 
-    
     '''
 
-    pass
+    result = math.pow(x, 3) - ((2 * x) + math.pow(x, 2)) - 100
+    return result
 
 def expression_2(x, y):
 
@@ -29,14 +29,29 @@ def expression_2(x, y):
         Write code that returns only the integer value of the following expression: (x^4 / 2y) - (3xy) + (6y / 7x)
     '''
 
-    pass
+    result = (math.pow(x, 4) / (2 * y)) - (3 * x * y) + ((6 * y) / (7 * x))
+    return int(result)
 
 
 def expression_3(x, y):
+
     '''
         Write code that returns the value of the following expression: (x^3 + y^2) / (x^2 - y^3)
     '''
 
+    result = (math.pow(x, 3) + math.pow(y, 2)) / (math.pow(x, 2) - math.pow(y, 3))
+    return result
+
 if __name__ == "__main__":
     
-    pass
+    val_1 = float(input("Enter a number for expression 1: "))
+    print(f"The answer to expression 1 is: {expression_1(val_1)}")
+
+    val_2 = float(input("Enter x for expression 2: "))
+    val_3 = float(input("Enter y for expression 2: "))
+    print(f"The answer to expression 2 is: {expression_2(val_2, val_3)}")
+
+    val_4 = float(input("Enter x for expression 3: "))
+    val_5 = float(input("Enter y for expression 3: "))
+    print(f"The answer to expression 3 is: {expression_3(val_4, val_5)}")
+
